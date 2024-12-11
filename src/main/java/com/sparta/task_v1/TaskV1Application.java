56fc7a56cab732baa7fe750c32dc49dacbcb2cc7
DESKTOP-APA79D3\kimmi <kimminsik0726@gmail.com>
2024-12-11 17:29:10 +0900

    Initial commit

---
+++ src/main/java/com/sparta/task_v1/TaskV1Application.java
@@ -0,0 +1,13 @@
+package com.sparta.task_v1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskV1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaskV1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/task_v1/TaskV1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.task_v1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskV1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
